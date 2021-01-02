64b9000f969986630d3ec37182ae761a4d7bb41e
abdelali <abdelali54abouelhassan@gmail.com>
2021-01-02 14:18:18 +0100

    testing ip

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,24 @@ use Illuminate\Http\Request;
 // });
 
 Route::get('/test', function (Request $request) {
-  return   'User IP Address - ' . $_SERVER['REMOTE_ADDR'];  
-
+    function getIPAddress()
+    {
+        //whether ip is from the share internet  
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        }
+        //whether ip is from the proxy  
+        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+        //whether ip is from the remote address  
+        else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }
+    $ip = getIPAddress();
+    return 'User Real IP Address - ' . $ip; 
     
 });
 
